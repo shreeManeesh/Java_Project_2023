aa3cc00a1c8994ee0f4f6fe791ef39acb6dc63ee
Sree Maneesh <Shreemaneesh2408@outlook.com>
2023-02-09 16:12:30 +0530

    day02
    Java chareters

---
+++ src/day02/Program02.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		
+		public static int fun()
+		{
+			int x=10;
+			System.out.println(x*x);
+			return x*x;
+		}
+
+		
+		public static void main(String[] args) {
+			long a=fun();//64,32//100.0
+			System.out.println(a+a);//200.0
+		}
+	
+
+}
