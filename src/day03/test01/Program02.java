471f4d2883dc6604c6d42b91bf6deaf9b7dad67b
Sree Maneesh <Shreemaneesh2408@outlook.com>
2023-02-09 16:17:11 +0530

    day03.test01
    test program02

---
+++ src/day03/test01/Program02.java
@@ -0,0 +1,12 @@
+package day03.test01;
+
+public class Program02 extends Program01{
+
+	public static void main(String[] args) {
+		Program02 xyz= new Program02(); 
+		xyz.addMethod(10, 20);
+		subMethod(20, 10);
+		mulMethod(20, 30);
+	}
+
+}
