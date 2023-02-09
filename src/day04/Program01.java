6ccbc088a3b1674071f429bf489e34fd80bb8948
Sree Maneesh <Shreemaneesh2408@outlook.com>
2023-02-09 16:50:58 +0530

    day04
    call

---
+++ src/day04/Program01.java
@@ -0,0 +1,22 @@
+package day04;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		
+		cal(10,20,30);
+		cal(12,13,15);
+		cal(17,18,11);
+
+	}
+     
+	public static void cal(int a,int b,int c) {
+		
+		System.out.println(a+b+c);
+		System.out.println((a-b)*c);
+		System.out.println(a*b*c);
+		
+		
+		
+	}
+}
